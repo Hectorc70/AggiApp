082aeaa8ed627938f754ad17778e0b2e9408d98d
Jero995 <jeronimo-1995x@hotmail.com>
2022-10-27 04:47:10 -0500

    HomeScreen Updated, Firebase Services added

--- lib/utill/app_constants.dart
+++ lib/utill/app_constants.dart
@@ -3,7 +3,7 @@ import 'package:flutter_sixvalley_ecommerce/data/model/response/language_model.d
 class AppConstants {
   static const String APP_NAME = 'Aggi';
   static const String APP_VERSION = '12.0';
-  static const String BASE_URL = 'https://6valley.6amtech.com';
+  static const String BASE_URL = 'https://aggicolombia.com/';
   static const String USER_ID = 'userId';
   static const String NAME = 'name';
   static const String CATEGORIES_URI = '/api/v1/categories';
